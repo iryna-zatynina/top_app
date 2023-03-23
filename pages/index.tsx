0ac742049a8b10247bd4d90bd053178dcccb10fe
iryna-zatynina <ira.zat1997@gmail.com>
2023-03-23 13:32:43 +0100

    add rating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
 import {Button, Htag} from "../components";
 import Ptag from "../components/Ptag/Ptag";
 import Tag from "../components/Tag/Tag";
+import {useEffect, useState} from "react";
+import Rating from "../components/Rating/Rating";
 
 
 export default function Home(): JSX.Element {
+    const [rating, setRating] = useState<number>(4);
+
     return (
         <>
-            <Htag tag='h1'>Text</Htag>
-            <Button appearance='primary' className='cclas' arrow='right'>Text</Button>
-            <Button appearance='ghost' className='cclas' arrow='down'>Text</Button>
+            <Htag tag='h1'>counter</Htag>
+            <Button appearance='primary' arrow='right' >Text</Button>
+            <Button appearance='ghost' arrow='down'>Text</Button>
             <Ptag size='small'>hello</Ptag>
             <Ptag size='medium'>hello</Ptag>
             <Ptag size='large'>hello</Ptag>
             <Ptag>hello</Ptag>
             <Tag color='green'>fff</Tag>
             <Tag color='primary' size="small">fff</Tag>
+            <Rating rating={rating} isEditable setRating={setRating}></Rating>
         </>
     );
 }
