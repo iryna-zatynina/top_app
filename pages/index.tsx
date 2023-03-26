4b994f33825df1d995d85422ea4ab0083506bbbd
iryna-zatynina <ira.zat1997@gmail.com>
2023-03-26 13:13:54 +0200

    add layout template and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import {Button, Htag} from "../components";
 import Ptag from "../components/Ptag/Ptag";
 import Tag from "../components/Tag/Tag";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import Rating from "../components/Rating/Rating";
+import {withLayout} from "../layout/Layout";
 
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
     const [rating, setRating] = useState<number>(4);
 
     return (
@@ -23,3 +24,5 @@ export default function Home(): JSX.Element {
         </>
     );
 }
+
+export default withLayout(Home);
