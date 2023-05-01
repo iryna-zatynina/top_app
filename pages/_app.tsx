9244e222d4f3b6bafab3e31c87194ec0f8a3b8b6
iryna-zatynina <ira.zat1997@gmail.com>
2023-05-01 21:57:23 +0200

    test husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,8 @@ export default function App({ Component, pageProps, router }: AppProps): JSX.Ele
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
-      c
+      <meta property="og:locale" content="ru_RU"/>
+
     </Head>
     <Component {...pageProps} />
   </>;
